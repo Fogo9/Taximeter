911d10748c56479e2d05da015402fc367b7800e2
Tuncay Demir <98576037+Fogo9@users.noreply.github.com>
2022-04-13 10:42:41 +0300

    Update Taximeter.java

--- Taximeter.java
+++ Taximeter.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 
 public class Taximeter {
+    
     public static void main(String[] args) {
 
         int km;
 
         double perKm = 2.20, total, startPrice = 10;
+        
 
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter Distance in KM : ");
 
         km = input.nextInt();
+        
 
         total = (km * perKm);
 
